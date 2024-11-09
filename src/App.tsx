bd12d2cf0fb3e069fda61950627f0ab398fcb49b
Miguel <miguelsribeiro7@gmail.com>
2024-11-09 00:10:24 +0000

    dark-mode setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
+import { useEffect, useState, useCallback } from "react";
 import { AppSidebar } from "@/components/sidebar-content";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
 function App() {
+	const [isDark, setIsDark] = useState(() => {
+		const stored = localStorage.getItem("isDark");
+		return stored ? JSON.parse(stored) : false;
+	});
+
+	useEffect(() => {
+		const stored = localStorage.getItem("isDark");
+		if (stored && JSON.parse(stored) !== isDark) {
+			setIsDark(JSON.parse(stored));
+		}
+	}, [isDark]);
+
+	// The callback is a fix for a weird re-render
+	const handleInput = useCallback(() => {
+		setIsDark((prevIsDark: boolean) => {
+			const newIsDark = !prevIsDark;
+			localStorage.setItem("isDark", JSON.stringify(newIsDark));
+			return newIsDark;
+		});
+	}, []);
+
 	return (
 		<>
-			<SidebarProvider>
-				<AppSidebar />
-				<SidebarInset>
-					<div className="w-full h-screen flex items-center justify-center">
-						<h1 className="text-3xl">hello world!!</h1>
-					</div>
-				</SidebarInset>
-			</SidebarProvider>
+			<div className={`${isDark ? "dark" : ""} transition-colors duration-300`}>
+				<SidebarProvider>
+					<AppSidebar isDark={isDark} setIsDark={handleInput} />
+					<SidebarInset>
+						<div className="w-full h-screen flex items-center justify-center">
+							<h1 className="text-3xl">hello world!!</h1>
+						</div>
+					</SidebarInset>
+				</SidebarProvider>
+			</div>
 		</>
 	);
 }
--- src/components/sidebar-content.tsx
+++ src/components/sidebar-content.tsx
@@ -1,5 +1,5 @@
-import { GalleryVerticalEnd } from "lucide-react";
-
+// sidebar-content.tsx
+import { Moon, Sun } from "lucide-react";
 import {
 	Sidebar,
 	SidebarContent,
@@ -11,165 +11,53 @@ import {
 	SidebarMenuSub,
 	SidebarMenuSubButton,
 	SidebarMenuSubItem,
-	SidebarRail,
 } from "@/components/ui/sidebar";
 
-// This is sample data.
-const data = {
-	navMain: [
-		{
-			title: "Getting Started",
-			url: "#",
-			items: [
-				{
-					title: "Installation",
-					url: "#",
-				},
-				{
-					title: "Project Structure",
-					url: "#",
-				},
-			],
-		},
-		{
-			title: "Building Your Application",
-			url: "#",
-			items: [
-				{
-					title: "Routing",
-					url: "#",
-				},
-				{
-					title: "Data Fetching",
-					url: "#",
-					isActive: true,
-				},
-				{
-					title: "Rendering",
-					url: "#",
-				},
-				{
-					title: "Caching",
-					url: "#",
-				},
-				{
-					title: "Styling",
-					url: "#",
-				},
-				{
-					title: "Optimizing",
-					url: "#",
-				},
-				{
-					title: "Configuring",
-					url: "#",
-				},
-				{
-					title: "Testing",
-					url: "#",
-				},
-				{
-					title: "Authentication",
-					url: "#",
-				},
-				{
-					title: "Deploying",
-					url: "#",
-				},
-				{
-					title: "Upgrading",
-					url: "#",
-				},
-				{
-					title: "Examples",
-					url: "#",
-				},
-			],
-		},
-		{
-			title: "API Reference",
-			url: "#",
-			items: [
-				{
-					title: "Components",
-					url: "#",
-				},
-				{
-					title: "File Conventions",
-					url: "#",
-				},
-				{
-					title: "Functions",
-					url: "#",
-				},
-				{
-					title: "next.config.js Options",
-					url: "#",
-				},
-				{
-					title: "CLI",
-					url: "#",
-				},
-				{
-					title: "Edge Runtime",
-					url: "#",
-				},
-			],
-		},
-		{
-			title: "Architecture",
-			url: "#",
-			items: [
-				{
-					title: "Accessibility",
-					url: "#",
-				},
-				{
-					title: "Fast Refresh",
-					url: "#",
-				},
-				{
-					title: "Next.js Compiler",
-					url: "#",
-				},
-				{
-					title: "Supported Browsers",
-					url: "#",
-				},
-				{
-					title: "Turbopack",
-					url: "#",
-				},
-			],
-		},
-		{
-			title: "Community",
-			url: "#",
-			items: [
-				{
-					title: "Contribution Guide",
-					url: "#",
-				},
-			],
-		},
-	],
-};
+import sidebarData from "@/data/sidebar-data.json";
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+interface AppSidebarProps {
+	isDark: boolean;
+	setIsDark: (value: boolean) => void;
+}
+
+export function AppSidebar({ isDark, setIsDark }: AppSidebarProps) {
 	return (
-		<Sidebar {...props}>
+		<Sidebar className="dark:border-red-primary/60 dark:bg-background dark:text-foreground">
 			<SidebarHeader>
 				<SidebarMenu>
 					<SidebarMenuItem>
-						<SidebarMenuButton size="lg" asChild>
+						<SidebarMenuButton className="justify-between" size="lg" asChild>
 							<a href="/">
-								<div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-									<GalleryVerticalEnd className="size-4" />
+								<div className="flex">
+									<div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+										<img src="/memoji.png" alt="memoji" />
+									</div>
+									<div className="flex flex-col gap-0.5 leading-none">
+										<span className="font-semibold">Portfolio</span>
+										<span className="text-xs">v2.0.0 (November, 2024)</span>
+									</div>
 								</div>
-								<div className="flex flex-col gap-0.5 leading-none">
-									<span className="font-semibold">Documentation</span>
-									<span className="">v1.0.0</span>
-								</div>
+								<button
+									onClick={(e) => {
+										e.preventDefault();
+										setIsDark(!isDark);
+									}}
+									type="button"
+								>
+									{isDark ? (
+										<Sun
+											size={20}
+											strokeWidth={1}
+											className="text-foreground"
+										/>
+									) : (
+										<Moon
+											size={20}
+											strokeWidth={1}
+											className="text-foreground"
+										/>
+									)}
+								</button>
 							</a>
 						</SidebarMenuButton>
 					</SidebarMenuItem>
@@ -178,16 +66,16 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 			<SidebarContent>
 				<SidebarGroup>
 					<SidebarMenu>
-						{data.navMain.map((item) => (
-							<SidebarMenuItem key={item.title}>
+						{sidebarData.navMain.map((section) => (
+							<SidebarMenuItem key={section.title}>
 								<SidebarMenuButton asChild>
-									<a href={item.url} className="font-medium">
-										{item.title}
+									<a href={section.url} className="font-medium">
+										{section.title}
 									</a>
 								</SidebarMenuButton>
-								{item.items?.length ? (
+								{section.items?.length ? (
 									<SidebarMenuSub>
-										{item.items.map((item) => (
+										{section.items.map((item) => (
 											<SidebarMenuSubItem key={item.title}>
 												<SidebarMenuSubButton asChild isActive={item.isActive}>
 													<a href={item.url}>{item.title}</a>
@@ -201,7 +89,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 					</SidebarMenu>
 				</SidebarGroup>
 			</SidebarContent>
-			<SidebarRail />
 		</Sidebar>
 	);
 }
