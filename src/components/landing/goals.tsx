4c03dd482b21ea5cb669bf3e3f901e27425481cb
Miguel <miguelsribeiro7@gmail.com>
2025-03-12 20:47:28 +0000

    fixed padding on mobile

--- src/components/landing/goals.tsx
+++ src/components/landing/goals.tsx
@@ -1,79 +1,79 @@
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 
 export function Goals() {
-	const goals = [
-		{
-			image:
-				"https://personal-static-files-cdn.fra1.cdn.digitaloceanspaces.com/Portfolio/SVGS/Astro.svg",
-			goal: "Learn Astro",
-			dark: true,
-		},
-		{
-			image:
-				"https://personal-static-files-cdn.fra1.cdn.digitaloceanspaces.com/Portfolio/SVGS/Three.js.svg",
-			goal: "Learn Three.js",
-			dark: true,
-		},
-		{
-			image: "/robot.svg",
-			goal: "Go deeper into Machine Learning",
-			dark: true,
-		},
-		{
-			image:
-				"https://personal-static-files-cdn.fra1.cdn.digitaloceanspaces.com/Portfolio/SVGS/Rust.svg",
-			goal: "Learn Rust",
-			dark: true,
-		},
-	];
+  const goals = [
+    {
+      image:
+        'https://personal-static-files-cdn.fra1.cdn.digitaloceanspaces.com/Portfolio/SVGS/Astro.svg',
+      goal: 'Learn Astro',
+      dark: true,
+    },
+    {
+      image:
+        'https://personal-static-files-cdn.fra1.cdn.digitaloceanspaces.com/Portfolio/SVGS/Three.js.svg',
+      goal: 'Learn Three.js',
+      dark: true,
+    },
+    {
+      image: '/robot.svg',
+      goal: 'Go deeper into Machine Learning',
+      dark: true,
+    },
+    {
+      image:
+        'https://personal-static-files-cdn.fra1.cdn.digitaloceanspaces.com/Portfolio/SVGS/Rust.svg',
+      goal: 'Learn Rust',
+      dark: true,
+    },
+  ];
 
-	return (
-		<div className="py-16 mt-[300px] md:mt-[200px]" id="4">
-			<div className="w-[80%] mx-auto px-4">
-				<motion.h1
-					initial={{ opacity: 0, y: -20 }}
-					animate={{ opacity: 1, y: 0 }}
-					className="text-4xl font-bold text-center"
-				>
-					MY GOALS
-					<p className="text-base font-normal text-center md:mb-32 mb-24 mt-10">
-						Everything I wish to acomplish in the nearby future
-					</p>
-				</motion.h1>
+  return (
+    <div className="py-16 mt-[400px] md:mt-[200px] " id="4">
+      <div className="w-[80%] mx-auto px-4">
+        <motion.h1
+          initial={{ opacity: 0, y: -20 }}
+          animate={{ opacity: 1, y: 0 }}
+          className="text-4xl font-bold text-center"
+        >
+          MY GOALS
+          <p className="text-base font-normal text-center md:mb-32 mb-24 mt-10">
+            Everything I wish to acomplish in the nearby future
+          </p>
+        </motion.h1>
 
-				<div className="relative">
-					<div className="absolute left-1/2 transform -translate-x-1/2 h-full w-0.5 bg-gradient-to-b from-red-400 to-red-primary" />
+        <div className="relative">
+          <div className="absolute left-1/2 transform -translate-x-1/2 h-full w-0.5 bg-gradient-to-b from-red-400 to-red-primary" />
 
-					{goals.map((goal, index) => (
-						<motion.div
-							initial={{ opacity: 0, x: index % 2 === 0 ? -50 : 50 }}
-							whileInView={{ opacity: 1, x: 0 }}
-							transition={{ duration: 0.5, delay: index * 0.2 }}
-							viewport={{ once: true }}
-							key={goal.goal}
-							className="mb-12 flex items-center w-full"
-						>
-							<div className="w-1/2 text-right pr-14 md:pr-20 flex items-end justify-end">
-								<div className="flex flex-col items-end p-6 dark:bg-black/10 dark:shadow-red-primary/70 dark:shadow-sm dark:hover:shadow-lg rounded-xl shadow-lg hover:shadow-xl dark:hover:shadow-red-primary/20 transition-shadow duration-300">
-									<img
-										src={goal.image}
-										className={`md:w-12 w-10 ${
-											goal.dark ? "dark:invert" : "invert-0"
-										} `}
-										alt="One of my goals"
-									/>
-								</div>
-							</div>
+          {goals.map((goal, index) => (
+            <motion.div
+              initial={{ opacity: 0, x: index % 2 === 0 ? -50 : 50 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: index * 0.2 }}
+              viewport={{ once: true }}
+              key={goal.goal}
+              className="mb-12 flex items-center w-full"
+            >
+              <div className="w-1/2 text-right pr-14 md:pr-20 flex items-end justify-end">
+                <div className="flex flex-col items-end p-6 dark:bg-black/10 dark:shadow-red-primary/70 dark:shadow-sm dark:hover:shadow-lg rounded-xl shadow-lg hover:shadow-xl dark:hover:shadow-red-primary/20 transition-shadow duration-300">
+                  <img
+                    src={goal.image}
+                    className={`md:w-12 w-10 ${
+                      goal.dark ? 'dark:invert' : 'invert-0'
+                    } `}
+                    alt="One of my goals"
+                  />
+                </div>
+              </div>
 
-							<div className="w-1/2 flex text-sm md:text-base justify-start md:pl-20 pl-10 relative">
-								<motion.p whileHover={{ scale: 1.2 }} className="font-semibold">
-									{goal.goal}
-								</motion.p>
-							</div>
-						</motion.div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+              <div className="w-1/2 flex text-sm md:text-base justify-start md:pl-20 pl-10 relative">
+                <motion.p whileHover={{ scale: 1.2 }} className="font-semibold">
+                  {goal.goal}
+                </motion.p>
+              </div>
+            </motion.div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/landing/tools.tsx
+++ src/components/landing/tools.tsx
@@ -1,85 +1,85 @@
 export function Tools() {
-	const cdnEndpoint = import.meta.env.VITE_CDN_ENDPOINT;
+  const cdnEndpoint = import.meta.env.VITE_CDN_ENDPOINT;
 
-	const technologies = [
-		{
-			name: "React",
-			logo: `${cdnEndpoint}/Portfolio/SVGS/React.svg`,
-		},
-		{
-			name: "TypeScript",
-			logo: `${cdnEndpoint}/Portfolio/SVGS/TypeScript.svg`,
-		},
-		{
-			name: "Node.js",
-			logo: `${cdnEndpoint}/Portfolio/SVGS/NodeJS.svg`,
-		},
-		{
-			name: "Tailwind CSS",
-			logo: `${cdnEndpoint}/Portfolio/SVGS/TailwindCSS.svg`,
-		},
-		{
-			name: "MongoDB",
-			logo: `${cdnEndpoint}/Portfolio/SVGS/MongoDB.svg`,
-		},
-		{
-			name: "ExpressJS",
-			logo: `${cdnEndpoint}/Portfolio/SVGS/ExpressJS.svg`,
-			invertD: true,
-		},
-		{
-			name: "PostgreSQL",
-			logo: `${cdnEndpoint}/Portfolio/SVGS/PostgresSQL.svg`,
-		},
-		{
-			name: "ShadCN",
-			logo: `${cdnEndpoint}/Portfolio/SVGS/shadcn-ui.svg`,
-			invert: true,
-		},
-		{
-			name: "Angular",
-			logo: `${cdnEndpoint}/Portfolio/SVGS/Angular.svg`,
-		},
-		{
-			name: "JavaScript",
-			logo: `${cdnEndpoint}/Portfolio/SVGS/Js.svg`,
-		},
-		{
-			name: "NextJS",
-			logo: `${cdnEndpoint}/Portfolio/SVGS/NextJS(2).svg`,
-			invert: true,
-		},
-	];
+  const technologies = [
+    {
+      name: 'React',
+      logo: `${cdnEndpoint}/Portfolio/SVGS/React.svg`,
+    },
+    {
+      name: 'TypeScript',
+      logo: `${cdnEndpoint}/Portfolio/SVGS/TypeScript.svg`,
+    },
+    {
+      name: 'Node.js',
+      logo: `${cdnEndpoint}/Portfolio/SVGS/NodeJS.svg`,
+    },
+    {
+      name: 'Tailwind CSS',
+      logo: `${cdnEndpoint}/Portfolio/SVGS/TailwindCSS.svg`,
+    },
+    {
+      name: 'MongoDB',
+      logo: `${cdnEndpoint}/Portfolio/SVGS/MongoDB.svg`,
+    },
+    {
+      name: 'ExpressJS',
+      logo: `${cdnEndpoint}/Portfolio/SVGS/ExpressJS.svg`,
+      invertD: true,
+    },
+    {
+      name: 'PostgreSQL',
+      logo: `${cdnEndpoint}/Portfolio/SVGS/PostgresSQL.svg`,
+    },
+    {
+      name: 'ShadCN',
+      logo: `${cdnEndpoint}/Portfolio/SVGS/shadcn-ui.svg`,
+      invert: true,
+    },
+    {
+      name: 'Angular',
+      logo: `${cdnEndpoint}/Portfolio/SVGS/Angular.svg`,
+    },
+    {
+      name: 'JavaScript',
+      logo: `${cdnEndpoint}/Portfolio/SVGS/Js.svg`,
+    },
+    {
+      name: 'NextJS',
+      logo: `${cdnEndpoint}/Portfolio/SVGS/NextJS(2).svg`,
+      invert: true,
+    },
+  ];
 
-	return (
-		<div className="w-full h-screen flex items-center justify-center ">
-			<div className="md:max-w-[60%]">
-				<h2
-					className="md:text-5xl text-center text-3xl uppercase font-semibold mb-20 md:mt-0 md:mb-32 pt-[350px] md:pt-[200px]"
-					id="3"
-				>
-					My Current Tools
-				</h2>
-				<div className="grid grid-cols-2 m-auto md:grid-cols-3 lg:grid-cols-4 gap-8">
-					{technologies.map((tech) => (
-						<div
-							key={tech.name}
-							className="group flex flex-col items-center justify-center p-4 rounded-lg shadow-md dark:shadow-sm dark:bg-black/10 dark:shadow-red-primary/60 dark:hover:shadow-red-primary/20 dark:hover:shadow-lg hover:shadow-lg transition-all duration-300"
-						>
-							<img
-								src={tech.logo}
-								alt={tech.name}
-								className={`md:w-32 w-24 h-16 mb-4 ${
-									tech.invert ? "dark:invert invert-0" : ""
-								} ${tech.invertD ? "invert dark:invert-0" : ""}`}
-							/>
-							<span className="dark:md:text-lg text-sm md:text-base font-medium transition-all duration-300">
-								{tech.name}
-							</span>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="w-full h-screen flex items-center justify-center mt-10 md:mt-0">
+      <div className="md:max-w-[60%]">
+        <h2
+          className="md:text-5xl text-center text-3xl uppercase font-semibold mb-20 md:mt-0 md:mb-32 pt-[350px] md:pt-[200px]"
+          id="3"
+        >
+          My Current Tools
+        </h2>
+        <div className="grid grid-cols-2 m-auto md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {technologies.map((tech) => (
+            <div
+              key={tech.name}
+              className="group flex flex-col items-center justify-center p-4 rounded-lg shadow-md dark:shadow-sm dark:bg-black/10 dark:shadow-red-primary/60 dark:hover:shadow-red-primary/20 dark:hover:shadow-lg hover:shadow-lg transition-all duration-300"
+            >
+              <img
+                src={tech.logo}
+                alt={tech.name}
+                className={`md:w-32 w-24 h-16 mb-4 ${
+                  tech.invert ? 'dark:invert invert-0' : ''
+                } ${tech.invertD ? 'invert dark:invert-0' : ''}`}
+              />
+              <span className="dark:md:text-lg text-sm md:text-base font-medium transition-all duration-300">
+                {tech.name}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
